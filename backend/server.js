d2a3dfbeb4e598a477a1a44b9fbd4242f4d64ae6
tvpeter <withtvpeter@gmail.com>
2022-09-05 14:44:07 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import  { chats } from './data/data.js'
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+    res.send("app is running");
+})
+
+app.get("/chats", (req, res) => {
+    res.send(chats);
+})
+
+app.get("/chats/:id", (req, res) => {
+    // console.log(req.params.id);
+    const chat = chats.find((c) => c._id == req.params.id);
+    res.send(chat);
+})
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+});
