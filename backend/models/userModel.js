5efa1674a46b855c5297d6b60000d42a58d32057
tvpeter <withtvpeter@gmail.com>
2022-09-06 17:30:13 +0100

    add models

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    pic: {
+        type: String,
+        default: 'https://img.icons8.com/ios-glyphs/30/000000/user--v1.png',
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+},
+{
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+
+export {User };
