367bafe5eb6a8a5d675ac543de2452bf92606930
tvpeter <withtvpeter@gmail.com>
2022-09-09 16:23:05 +0100

    add creating a chat group

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -68,5 +68,39 @@ const fetchChats = expressAsyncHandler(async (req, res) => {
         throw new Error(error.message);
     }
 });
+const createGroupChat = expressAsyncHandler(async (req, res) => {
+    if (!req.body.users || !req.body.name) {
+        return res.status(400).send({ message: "please fill all the fields" });
+    }
 
-export { accessChat, fetchChats };
+    const users = JSON.parse(req.body.users);
+
+    if (users.length < 2) {
+        return res.status(400).send("more than 2 users are required to form a group chat");
+    }
+
+    users.push(req.user);
+
+    try {
+        const groupChat = await Chat.create({
+            chatName: req.body.name,
+            users,
+            isGroupChat: true,
+            groupAdmin: req.user,
+        });
+
+        const fullGroupChat = await Chat.findOne({
+            _id: groupChat._id
+        })
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password");
+        
+        res.status(200).json(fullGroupChat);
+        
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+})
+
+export { accessChat, fetchChats, createGroupChat };
