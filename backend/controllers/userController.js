d04aceb517521eaf39ef31d1359abec1b6d7ba98
tvpeter <withtvpeter@gmail.com>
2022-09-09 03:59:31 +0100

    add searching user api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,4 +61,16 @@ const authUser = asyncHandler(async (req, res) => {
 
 });
 
-export { registerNewUser, authUser };
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search ? {
+    $or: [
+      { name: { $regex: req.query.search, $options: "i" } },
+      { email: { $regex: req.query.search, $options: "i" } },
+    ],
+  } : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+})
+
+export { registerNewUser, authUser, allUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import { registerNewUser, authUser }  from '../controllers/userController.js';
+import { registerNewUser, authUser, allUsers } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route("/").post(registerNewUser)
+router.route("/").post(registerNewUser).get(protect, allUsers);
 router.post("/login", authUser)
+// router.route("/").get(allUsers);
 
 export { router };
