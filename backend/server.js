6fc0f5fc7dc5c03c92810c1e5b219bbe8d7b0d65
tvpeter <withtvpeter@gmail.com>
2022-09-09 02:39:26 +0100

    add error middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 
 import { connectDB } from './config/db.js';
 import { router as userRoutes } from './routes/userRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 const app = express();
 dotenv.config();
@@ -17,6 +18,9 @@ app.get("/", (req, res) => {
     res.send("app is running");
 })
 app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`.yellow.bold);
