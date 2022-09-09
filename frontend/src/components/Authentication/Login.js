13055dcfbce848e2c82695d87862cae29e41c2e4
tvpeter <withtvpeter@gmail.com>
2022-09-09 03:28:46 +0100

    add login functionality

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,11 +1,17 @@
-import { FormControl, FormLabel, Input, VStack, InputGroup, InputRightElement, Button } from '@chakra-ui/react';
-import React, { useState } from 'react'
+import { FormControl, FormLabel, Input, VStack, InputGroup, InputRightElement, Button, useToast } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import axios from "axios";
+import { useHistory } from "react-router";
 
 const Login = () => {
 
     const [show, setShow] = useState(false);
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const toast = useToast();
+    const history = useHistory();
+    const [loading, setLoading] = useState(false);
+
 
  
     const handleClick = () => setShow(!show);
@@ -13,7 +19,57 @@ const Login = () => {
     const postDetails = (pics) => {
 
     }
-    const submitHandler = () => {}
+    const submitHandler = async () => {
+        setLoading(true);
+        if (!email || !password) {
+            toast({
+                title: "Please fill all the fields",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom"
+            });
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                }
+            };
+
+            const { data } = await axios.post("/api/user/login",
+                {
+                    email, password
+                },
+                config
+            );
+
+            toast({
+                title: "Login successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom"
+            });
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            history.push("/chats");
+
+        } catch (error) {
+            toast({
+                title: "Error occured",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom"
+            });
+            setLoading(false);
+        }
+    }
 
   return (
       <VStack spacing="5px" color="black">
@@ -43,7 +99,9 @@ const Login = () => {
               colorScheme="blue"
               width="100%"
               style={{ marginTop: 15 }}
-              onClick={submitHandler}>
+              onClick={submitHandler}
+              isLoading={loading}
+          >
               Login
           </Button>
 
